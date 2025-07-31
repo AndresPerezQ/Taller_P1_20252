cbd489db3fd00b54841359078ae018c3b3845421
Andres Perez Quinchia <aperezq@eafit.edu.co>
2025-07-31 10:51:10 -0500

    Updated home function

--- movie/views.py
+++ movie/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse
 def home(request):
     ##return HttpResponse('<h1> Welcome to the Home Page </h1>')
     ##return render (request, 'home.html')
-    return render(request, 'home.html', {'name': 'Andrés Pérez Quinchía'})
+    return render(request, 'home.html', {'name': 'Andrés Pérez Quinchía :D'})
 
 def about(request):
     return HttpResponse('<h1> About Page by Andrés Pérez Quinchía <h1>')
